3eee95cff4f8fc3bfb8477772c1cd2f1ef416f63
Sondre Naustdal <sondre.naustdal@gmail.com>
2010-12-08 16:42:58 -0800

    cleaned up the code

--- src/main.go
+++ src/main.go
@@ -1,119 +1,130 @@
 package main
 
 import (
-// 	"fmt"
+	// 	"fmt"
 	"os"
 	"strings"
 	"strconv"
 )
 
-type Doc struct{
-	
+type Doc struct {
 	I int
 	T string
 	A string
 	B string
 	W string
-	
 }
 
-type DocMap map[int] Doc
+type DocMap map[int]Doc
 
 func contents(filename string) (string, os.Error) {
-    f, err := os.Open(filename, os.O_RDONLY, 0)
-    if err != nil {
-        return "", err
-    }
-    defer f.Close()  // f.Close will run when we're finished.
-
-    var result []byte
-    buf := make([]byte, 100)
-    for {
-        n, err := f.Read(buf[0:])
-        result = append(result, buf[0:n]...) // append is discussed later.
-        if err != nil {
-            if err == os.EOF {
-                break
-            }
-            return "", err  // f will be closed if we return here.
-        }
-    }
-    return string(result), nil // f will be closed if we return here.
-}
-func (dm DocMap)AddTo(a int, s string) (err os.Error){
-	
-	if len(s) == 0{
-		return
+	f, err := os.Open(filename, os.O_RDONLY, 0)
+	if err != nil {
+		return "", err
 	}
-	d := new(Doc)
-	
-	tmp := strings.Split(s,".T",-1)
-	
-// 	for i := range(tmp){
-// 		println(i,tmp[i])
-// 	}
+	defer f.Close() // f.Close will run when we're finished.
+
+	var result []byte
+	buf := make([]byte, 100)
+	for {
+		n, err := f.Read(buf[0:])
+		result = append(result, buf[0:n]...) // append is discussed later.
+		if err != nil {
+			if err == os.EOF {
+				break
+			}
+			return "", err // f will be closed if we return here.
+		}
+	}
+	return string(result), nil // f will be closed if we return here.
+}
+
+
+func sToDoc(s string) (*Doc, os.Error) {
+
+	s = strings.Replace(s, "\n", " ", -1)
+	tmp := strings.Split(s, ".T", -1)
+
 	tmp[0] = strings.TrimSpace(tmp[0])
-	I,_ := strconv.Atoi(tmp[0])
-// 	println("I = ",I)
-	
-	if I == 0 {
-		return
-	}
-	
-	tmp = strings.Split(tmp[1],".A",-1)
-	
-	T := strings.TrimSpace(tmp[0])
-	
-	tmp = strings.Split(tmp[1],".B",-1)
-	
-	A := strings.TrimSpace(tmp[0])
-	
-	tmp = strings.Split(tmp[1],".W",-1)
-	
-	B := strings.TrimSpace(tmp[0])
-	
-	W := strings.TrimSpace(tmp[1])
-	
-// 	println(I,T,A,B,W)
-	d.I = I
-	d.T = T
-	d.A = A
-	d.B = B
-	d.W = W
-	dm[I] = *d
-	
-	return
-}
-func DocReader() (i int, err os.Error) {
+	I, err := strconv.Atoi(tmp[0])
 
-	Docs , err := contents("../data/cran.all.1400")
+	if I == 0 {
+		return nil, os.NewError("Empty doc")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	tmp = strings.Split(tmp[1], ".A", -1)
+
+	T := strings.TrimSpace(tmp[0])
+
+	tmp = strings.Split(tmp[1], ".B", -1)
+
+	A := strings.TrimSpace(tmp[0])
+
+	tmp = strings.Split(tmp[1], ".W", -1)
+
+	B := strings.TrimSpace(tmp[0])
+
+	W := strings.TrimSpace(tmp[1])
+
+	return &Doc{I: I, T: T, A: A, B: B, W: W}, nil
+}
+
+
+func (dm DocMap) AddTo(s string) (err os.Error) {
+
+	if s == "" {
+		return os.NewError("Empty string")
+	}
+	d, err := sToDoc(s)
 
 	if err != nil {
-		return 0, err
+		return err
 	}
-	
-	DocStrings := strings.Split(Docs,".I",-1)
-	
-// 	println(DocStrings[3])
+
+	dm[d.I] = *d
+
+	return nil
+}
+func DocReader() (err os.Error) {
+
+	Docs, err := contents("../data/cran.all.1400")
+
+	if err != nil {
+		return
+	}
+
+	DocStrings := strings.Split(Docs, ".I", -1)
+
+	// 	println(DocStrings[3])
 	dm := make(DocMap)
-	for i = 0; i < len(DocStrings); i++{
-// 	for i = 0; i < 2; i++{
-		dm.AddTo(i,DocStrings[i])
-		
+	for i := range DocStrings {
+		// 	for i = 0; i < 2; i++{
+		dm.AddTo(DocStrings[i])
+
 	}
-	
-	
-	println("|",dm[52].I,"|")
-	println("|",dm[52].T,"|")
-	println("|",dm[52].A,"|")
-	println("|",dm[52].B,"|")
-	println("|",dm[52].W,"|")
-	return i, err
+
+	// 	println("|",dm[52].I,"|")
+	// 	println("|",dm[52].T,"|")
+	// 	println("|",dm[52].A,"|")
+	// 	println("|",dm[52].B,"|")
+	println("|", dm[52].W, "|")
+
+	for i := range dm {
+		// 	for i := 0 ; i < len(dm);i++{
+		if len(dm[i].A) < 4 {
+			println(dm[i].A, dm[i].I)
+		}
+	}
+
+	return nil
 }
 
 
 func main() {
 
 	DocReader()
-// 	println(i)
+	// 	println(i)
 }
