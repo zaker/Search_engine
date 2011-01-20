eb078e597f90ca9bbd2117601ddc69c3ec208a2a
Sondre Naustdal <sondre.naustdal@gmail.com>
2011-01-19 18:41:51 -0800

    added test webserver

--- src/main.go
+++ src/main.go
@@ -28,42 +28,8 @@ func main() {
 		println(err.String())
 	}
 
-	var words []string
-	for i := range im {
-		// 		key, _ := im[i]
-		// 		print(i, " : ")
-		// 
-		words = append(words, i)
-		// 		for j := range key {
-		// 		print(im.LenDocs(i))
-		// 		}
-		// 		println()
-	}
-	// 
 
-	// 	sa := sort.StringArray(words[0:])
-	sort.SortStrings(words)
-
-	// 	for i := range words {
-	// 		println(words[i])
-	// 	}
-	// 	println(len(im), len(words))
-	// // 	println(i)
-	// 	println(qm[4].W)
-	// // 	st map[string][]int
-	// 	for i := range qm[4].S{
-	// 		q := qm[4].S[i]
-	// // 		println()
-	// 
-	// 		a := im[q]
-	// 		
-	// 		for j:= range a{
-	// 			print(a[j],",")
-	// 		}
-	// 		println()
-	// 		
-	// 	}
-
-	QuerriesProc(dm, qm, im)
+// 	QuerriesProc(dm, qm, im)
+	wServer()
 
 }
